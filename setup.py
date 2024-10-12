e50827f2588531f1088711fc19150f51f52f663d
HX Wang <154685738+sleepy-bedless@users.noreply.github.com>
2024-10-12 16:15:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ module = Extension('_muse', sources = ['muse_wrap.cxx'])
 setup(name = 'muse',
       version = '0.2',
       author = 'HXW',
-      description = """提供简单的MIDI输出控件类""",
+      description = """simple classes for midi""",
       ext_modules = [module],
       py_modules = ['muse'])
 
